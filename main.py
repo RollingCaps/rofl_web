b4961d2759f5795f668567096b1ec604243dfe2c
RollingCaps <cptnormand@gmail.com>
2018-04-23 20:18:42 +0300

    Добавил больше статистики.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import mysql.connector
 import secret_config
+from statistics import mean, mode, variance
 
 app = Flask(__name__)
 
@@ -12,16 +13,16 @@ def index():
                                   host=secret_config.host, database=secret_config.database)
     cursor = cnx.cursor()
     cursor.execute("SELECT Text FROM Reposts")
-    lng = 0
-    cnt = 0
+    data = []
     cnt_empty = 0
     for (Text) in cursor:
-        lng += len(Text[0])
+        data.append(len(Text[0]))
         if len(Text[0]) == 0:
             cnt_empty += 1
-        cnt += 1
     cnx.close()
-    return render_template("index.html", avg_text=lng / cnt, with_text=cnt - cnt_empty, no_text=cnt_empty)
+    return render_template("index.html", cnt=len(data), avg_text=mean(data), md_text=mode(data), d_text=variance(data),
+                           min_text=min(data), max_text=max(data),
+                           with_text=len(data) - cnt_empty, no_text=cnt_empty)
 
 
 app.run(debug=True)
